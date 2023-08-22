97fc379da3fc84659c51bd70aa2510a7a3008cd3
Suraj <42367885+suraj0041@users.noreply.github.com>
2023-08-22 09:55:19 +0530

    DS question

--- Playground.js
+++ Playground.js
@@ -1,16 +1,18 @@
 function test() {
+  const input = [2, 7, 11, 4, -2];
+  const output = [20, 15, 11, 18, 24];
 
-  const input = [2, 7, 11, 4, -2]
   let newArr = [];
   for (let i = 0; i < input.length; i++) {
     debugger;
-    let arr1 = input.filter(x => x != input[i]);
-    newArr.push(arr1.reduce((acc, crr) => {
-      return acc += crr;
-    }, 0));
-
+    let arr1 = input.filter((x) => x != input[i]);
+    newArr.push(
+      arr1.reduce((acc, crr) => {
+        return (acc += crr);
+      }, 0)
+    );
   }
-  console.log(newArr);
-};
+  document.getElementById("div_result").innerText = newArr;
+}
 
 test();
